c43510b77478b567461c0726a343e68775204fbe
Sudhams Reddy Duba <support@way2techstack.com>
2025-10-07 14:12:32 +0530

    Example for remote state storage configuration

--- 6.Scenario05.Remote_Storage/resourcegroup.tf
+++ 6.Scenario05.Remote_Storage/resourcegroup.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "ado_rg_groups" {
+resource "azurerm_resource_group" "azure_resource_group" {
   for_each = var.resource_group_names
   name     = each.key
   location = each.value # Or a dynamic location from a map
